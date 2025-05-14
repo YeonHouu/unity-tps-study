ae528136c6c72a648dce6a1b4b21fa4a64d777d3
YeonHouu <yeonhhou@gmail.com>
2025-05-14 15:18:00 +0900

    🔥 chore: Remove test script

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -37,7 +37,23 @@ public class PlayerController : MonoBehaviour
 
     private void HandleMovement()
     {
-        // TODO: Movement 병합시 기능 추가예정
+        // (회전 수행 후)좌우 회전에 대한 벡터 반환
+        Vector3 camRotateDir = movement.SetAimRotation();
+
+        float moveSpeed;
+        if (status.IsAiming.Val) moveSpeed = status.WalkSpeed;
+        else moveSpeed = status.RunSpeed;
+
+        Vector3 moveDir = movement.SetMove(moveSpeed);
+        // 벡터가 제로가 아니라면 isMoving에 대입
+        status.IsMoving.Val = (moveDir != Vector3.zero);
+
+        Vector3 avatarDir;
+        // 카메라 에이밍 중이면 카메라회전 방향
+        if (status.IsAiming.Val) avatarDir = camRotateDir;
+        //움직이는 방향으로 변환
+        else avatarDir = moveDir;
+        movement.SetAvatarRotation(avatarDir);
     }
 
     private void HandleAiming()
