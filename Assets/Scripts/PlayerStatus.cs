4b2b384506160bb89fded7ad716e5560bceedbb9
YeonHouu <yeonhhou@gmail.com>
2025-05-14 11:37:23 +0900

    ✨ feat: Add player status management system
    - Add playerstatus for player's movements

---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DesignPattern;
+using UnityEditor.Build;
+
+public class PlayerStatus : MonoBehaviour
+{
+    [field: SerializeField][field: Range(0, 10)]
+    public float WalkSpeed { get; set; }
+
+    [field: SerializeField][field: Range(0, 10)]
+    public float RunSpeed { get; set; }
+
+    [field: SerializeField][field: Range(0, 10)]
+    public float RotatateSpeed { get; set; }
+
+    public ObservableProperty<bool> IsAiming { get; private set; } = new();
+    public ObservableProperty<bool> IsMoving { get; private set; } = new();
+    public ObservableProperty<bool> IsAttacking { get; private set; } = new();
+
+
+}
